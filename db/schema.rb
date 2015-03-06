7438e6dcd9b06b3c27e21822aa75de96a6bcdd5e
Edward Mitchell <edward.l.mitchell2@gmail.com>
2015-03-05 23:03:08 -0600

    Basic setup, SASS, nav partial. No fun stuff yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150306020529) do
 
   # These are extensions that must be enabled in order to support this database
@@ -38,5 +39,4 @@ ActiveRecord::Schema.define(version: 20150306020529) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 end
