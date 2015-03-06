8cb62aca3b17796cc28175dfb2c47861cdccf29f
Matthew Gray <graymatthew3@gmail.com>
2015-03-05 20:46:59 -0600

    add initial algorithm for ticket generation for a given promotion

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,35 @@
 class Promotion < ActiveRecord::Base
   has_many :tickets
+
+  after_create :generate_tickets
+
+  def self.minimum_discount
+    0.10
+  end
+
+  def generate_tickets
+    (min_group_size..max_group_size).each do |group_size|
+      ticket_params = {}
+      ticket_params[:group_size]      = group_size
+      ticket_params[:min_total_spend] = min_spend * group_size
+      ticket_params[:discount]        = calculate_discount(group_size)
+      ticket_params[:active]          = true
+
+      self.tickets.create(ticket_params)
+    end
+  end
+
+  private
+
+  def calculate_discount(group_size)
+    discount = discount_variance / max_group_size * group_size + self.class.minimum_discount
+    if preferred_group_size == group_size
+      discount += (max_discount / 10)
+    end
+    discount
+  end
+
+  def discount_variance
+    max_discount - self.class.minimum_discount
+  end
 end
