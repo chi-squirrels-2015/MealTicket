145bbf8437b5b4f623bde3857d3e3484f9361cd5
janeskim <kim.jane.sue@gmail.com>
2015-03-08 21:26:49 -0500

    small formatting change

--- db/migrate/20150308220910_create_purchases.rb
+++ db/migrate/20150308220910_create_purchases.rb
@@ -2,10 +2,10 @@ class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
       t.belongs_to :ticket
-      t.string :purchaser_name
-      t.string :phone_number
-      t.string :confirmation_id
-      t.paid :false
+      t.string     :purchaser_name
+      t.string     :phone_number
+      t.string     :confirmation_id
+      t.paid       :false
 
       t.timestamps null: false
     end
