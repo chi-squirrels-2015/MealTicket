dd3ec0432e226c791797fffd55c6dc223493a038
janeskim <kim.jane.sue@gmail.com>
2015-03-08 21:24:58 -0500

    create migration for purchases

--- db/migrate/20150308220910_create_purchases.rb
+++ db/migrate/20150308220910_create_purchases.rb
@@ -1,6 +1,11 @@
 class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
+      t.belongs_to :ticket
+      t.string :purchaser_name
+      t.string :phone_number
+      t.string :confirmation_id
+      t.paid :false
 
       t.timestamps null: false
     end
