68f3bb929755b0dfa1e848689d570eda4ee54c03
Andrew Irwin <andrew.irwin33@gmail.com>
2015-03-10 10:51:04 -0500

    added location control functionality to maps

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,32 +4,51 @@ $(document).ready(function(){
 
   var map = L.mapbox.map('map', 'airwin33.ldh7b6gb').locate({setView: true, maxZoom: 14});
 
-  map.on('locationfound', function(event){
-    var currentLoc = L.marker(event.latlng).addTo(map);
-    myLayer = L.mapbox.featureLayer()
-      .loadURL('/closest_restaurants?lat=' + currentLoc.getLatLng().lat + '&lng=' + currentLoc.getLatLng().lng)
-      .addTo(map);
+  var currentLoc = L.marker();
+  var markerLayer = L.mapbox.featureLayer(); 
 
-
-    myLayer.on('layeradd', function(e){
-    var marker = e.layer,
-      feature = marker.feature;
-
-    var content = e.layer.feature.properties
-    var content = '<p>' + content.name + '</p>' + '<p>' + content.address + '</p>' + '<form action="/restaurants/' + content.id + '"><input type="submit" value="View Promotions"></form>'
-
-    marker.bindPopup(content);
-   
-
-    });
-
-
-
-    myLayer.on('click', function(e) {
-        map.panTo(e.layer.getLatLng());
-    });
-
+  var geocoderControl = L.mapbox.geocoderControl('mapbox.places');
+  geocoderControl.addTo(map);
+  var locateControl = L.control.locate({drawCircle: false, keepCurrentZoomLevel: true, locateOptions: {maxZoom:14}}).addTo(map);
+ 
+  geocoderControl.on('found', function(response){ 
+    var lat = response.results.features[0].geometry.coordinates[1];
+    var lng = response.results.features[0].geometry.coordinates[0];
+    currentLoc.setLatLng([lat,lng]).addTo(map);
+      
+    markerLayer.clearLayers();
+    addMarkers();
   });
 
+  $('#geolocate').on("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    map.locate({setView: true});
+  });
 
+  map.on('locationfound', function(event){
+    currentLoc.setLatLng(event.latlng).addTo(map);
+    markerLayer.clearLayers();
+    addMarkers();
+  });
+
+  var addMarkers = function() {
+    markerLayer.loadURL('/closest_restaurants?lat=' + currentLoc.getLatLng().lat + '&lng=' + currentLoc.getLatLng().lng)
+      .addTo(map);
+
+    markerLayer.on('layeradd', function(e){
+      var marker = e.layer,
+          feature = marker.feature;
+
+      var content = e.layer.feature.properties
+      var content = '<p>' + content.name + '</p>' + '<p>' + content.address + '</p>' + '<form action="/restaurants/' + content.id + '"><input type="submit" value="View Promotions"></form>'
+
+      marker.bindPopup(content);
+
+    });
+
+    markerLayer.on('click', function(e) {
+      map.panTo(e.layer.getLatLng());
+    });
+  }
 });
