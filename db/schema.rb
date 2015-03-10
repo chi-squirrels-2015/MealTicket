64cc2dbd9938486df9e5bd1a84fe5df14aae6c66
Matthew Gray <graymatthew3@gmail.com>
2015-03-10 13:00:19 -0500

    remove errant users table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -67,17 +67,4 @@ ActiveRecord::Schema.define(version: 20150310170853) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
