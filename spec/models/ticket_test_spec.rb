2435b87a394cd90bc8339fda2bf05282c74b6b56
Edward Mitchell <edward.l.mitchell2@gmail.com>
2015-03-10 11:48:10 -0500

    Add ticket specs

---
+++ spec/models/ticket_test_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Ticket do
+  describe 'Deactivate' do
+    let!(:ticket) {Ticket.new(id: 1, promotion_id: 1, discount: 0.1, min_total_spend: 200, group_size: 4, active: true, ticket_price: 50, loss_per_ticket: 150)}
+
+    it "should be active when created" do
+      expect(ticket.active).to be (true)
+    end
+    it "should not be active when deactivated" do
+      ticket.deactivate!
+      expect(ticket.active).to be (false)
+    end
+  end
+  
+end
