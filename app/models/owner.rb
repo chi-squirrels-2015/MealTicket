b06cdfb8ab0dd2e32e17bc7b23adbca30972aa30
Andrew Irwin <andrew.irwin33@gmail.com>
2015-03-11 11:48:08 -0500

    remove devise and owner

--- app/models/owner.rb
+++
@@ -1,8 +0,0 @@
-class Owner < ActiveRecord::Base
-  has_one :restaurant
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
