dd6a41a014180240e4688c8ef72b523daa2303f9
Andrew Irwin <andrew.irwin33@gmail.com>
2015-03-11 11:44:31 -0500

    added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+    require 'pry'; binding.pry
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    require 'pry'; binding.pry
+    @restaurant = Restaurant.find_by_name(params[:name]).try(:authenticate, params[:password])
+    if @restaurant
+      p 'SETTING SESSION'
+      p @restaurant.id
+      session[:restaurant_id] = @restaurant.id
+      redirect_to dashboard_path
+    else
+      render login_path
+    end
+  end
+
+  def destroy
+    session.delete(:restaurant_id)
+    redirect_to signup_path
+  end
+
+end
