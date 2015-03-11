c3c42fd902ab1ccbf7a4c43e9a230a8b3951dbd6
janeskim <kim.jane.sue@gmail.com>
2015-03-11 10:29:11 -0500

    remove backbone skeleton, replaced with AJAX request

--- app/assets/javascripts/views/promotion_tickets_view.js
+++
@@ -1,7 +0,0 @@
-var PromotionTicketsView = Backbone.View.extend({
-  template: JST["templates/promotion_tickets"],
-
-  render: function() {
-    this.$el.html(this.template(this.model.attributes));
-  }
-});
