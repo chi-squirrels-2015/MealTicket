0e734d205cf68b5f4a271dccaeb0ef7870089412
Matthew Gray <graymatthew3@gmail.com>
2015-03-12 08:28:45 -0500

    remove index and search actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,14 +6,6 @@ class RestaurantsController < ApplicationController
   #### ADMIN SIDE ####
   ####################
 
-  def index
-    @restaurants = Restaurant.all
-    render json: @restaurants
-  end
-
-  def search
-  end
-
   def search_yelp
     parameters = { sort: "0", term: params["business-name"], limit: 3, category_filter: "restaurants,bars" }
     render json: Yelp.client.search(params["location"], parameters).businesses
