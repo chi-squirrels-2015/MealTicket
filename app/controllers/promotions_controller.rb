6ca01b043cceb1b51f1e0c2dad645f48a175a1a8
Matthew Gray <graymatthew3@gmail.com>
2015-03-12 08:28:16 -0500

    remove index action, protect new action, use current_restaurant

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,18 +1,15 @@
 class PromotionsController < ApplicationController
-  def index
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @promotions = @restaurant.promotions
-  end
-
   def new
+    unless current_restaurant
+      redirect_to root_path
+    end
     @promotion = Promotion.new
   end
 
   def create
     promo = promotion_params.to_h
     promo['max_discount'] = promotion_params['max_discount'].to_f / 100
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @promotion = @restaurant.promotions.build(promo)
+    @promotion = current_restaurant.promotions.build(promo)
 
     if @promotion.save
       redirect_to dashboard_path
@@ -24,10 +21,12 @@ class PromotionsController < ApplicationController
 
   def show
     @promotion = Promotion.find(params[:id])
-    @promotion.restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def preview_tickets
+    unless current_restaurant
+      redirect_to root_path
+    end
     promo = promotion_params.to_h
     promo['max_discount'] = promotion_params['max_discount'].to_f / 100
     @promotion = Promotion.new(promo)
@@ -40,7 +39,6 @@ class PromotionsController < ApplicationController
   end
 
   def promotion_params
-    puts params
     params.require(:promotion).permit(:name, :valid_on, :restaurant_id, :min_group_size, :max_group_size, :preferred_group_size, :max_discount, :min_spend, :loss_tolerance)
   end
 end
